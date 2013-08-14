00b776edfaf3a0a2753c9d73f59f362bb621a7da
Maple <hao.wan@tufts.edu>
2013-08-14 15:21:58 -0400

    asdf

--- src/test/java/com/trunk/selenium/trunkTestSuit/Test_suit.java
+++ src/test/java/com/trunk/selenium/trunkTestSuit/Test_suit.java
@@ -5,8 +5,8 @@ import junit.framework.TestSuite;
 public class Test_suit {
   public static TestSuite suite() {
     TestSuite suite = new TestSuite();
-    /**suite.addTestSuite(Trunk_test1_login.class);
-    suite.addTestSuite(Trunk_test2_upload.class);
+    suite.addTestSuite(Trunk_test1_login.class);
+    /**suite.addTestSuite(Trunk_test2_upload.class);
     suite.addTestSuite(Trunk_test3_tuftsNow.class);
     suite.addTestSuite(Trunk_test4_communication.class);
     suite.addTestSuite(Trunk_test5_mysetting.class); **/
