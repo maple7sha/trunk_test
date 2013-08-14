05c6942818deb2b0fcb7a8c9faa6b148f2f2ce63
Maple <hao.wan@tufts.edu>
2013-08-14 15:40:13 -0400

    include all

--- src/test/java/com/trunk/selenium/trunkTestSuit/Test_suit.java
+++ src/test/java/com/trunk/selenium/trunkTestSuit/Test_suit.java
@@ -5,7 +5,7 @@ import junit.framework.TestSuite;
 public class Test_suit {
   public static TestSuite suite() {
     TestSuite suite = new TestSuite();
-    suite.addTestSuite(Trunk_test1_login.class);
+    suite.addTestSuite(Trunk_test1_login);
     /**suite.addTestSuite(Trunk_test2_upload.class);
     suite.addTestSuite(Trunk_test3_tuftsNow.class);
     suite.addTestSuite(Trunk_test4_communication.class);
