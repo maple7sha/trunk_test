0e99e5075caa4ce07e8c5ba199fa5f73a111fceb
Maple <hao.wan@tufts.edu>
2013-08-15 10:57:10 -0400

    junit 4 test suit format used

--- src/test/java/com/trunk/selenium/trunkTestSuit/Test_suite.java
+++ src/test/java/com/trunk/selenium/trunkTestSuit/Test_suite.java
@@ -1,6 +1,26 @@
+package com.trunk.selenium.trunkTestSuit;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+Trunk_test1_login.class,
+Trunk_test2_upload.class,
+Trunk_test3_tuftsNow.class,
+Trunk_test4_communication.class,
+Trunk_test5_mysetting.class,
+})
+
+public class Test_suite{
+}
+
+
+/**
 import junit.framework.Test;
 import junit.framework.TestSuite;
 import com.trunk.selenium.trunkTestSuit.*;
+
 
 public class Test_suite {
   public static Test suite() {
@@ -18,5 +38,4 @@ public class Test_suite {
   }
 }
 
-
-
+**/
