c3048cd3167c0dd921a33550e258a400cdf915cb
Maple <hao.wan@tufts.edu>
2013-08-15 10:34:24 -0400

    from Testsuite to Test

--- src/test/java/com/trunk/selenium/trunkTestSuit/Test_suite.java
+++ src/test/java/com/trunk/selenium/trunkTestSuit/Test_suite.java
@@ -3,9 +3,9 @@ import junit.framework.TestSuite;
 import com.trunk.selenium.trunkTestSuit.*;
 
 public class Test_suite {
-  public static TestSuite suite() {
+  public static Test suite() {
     TestSuite suite = new TestSuite();
-    suite.addTestSuite(Trunk_test1_login);
+    suite.addTestSuite(Trunk_test1_login.class);
     suite.addTestSuite(Trunk_test2_upload.class);
     suite.addTestSuite(Trunk_test3_tuftsNow.class);
     suite.addTestSuite(Trunk_test4_communication.class);
