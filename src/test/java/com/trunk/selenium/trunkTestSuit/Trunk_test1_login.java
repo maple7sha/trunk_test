7d43cb1695f3834512b7b14116ed8528382467e5
Maple <hao.wan@tufts.edu>
2013-09-16 09:53:51 -0400

    test maven_opts and print user.name and user.supp(added param)

--- src/test/java/com/trunk/selenium/trunkTestSuit/Trunk_test1_login.java
+++ src/test/java/com/trunk/selenium/trunkTestSuit/Trunk_test1_login.java
@@ -22,7 +22,8 @@ public class Trunk_test1_login {
   @Test
   public void testTrunkTest1() throws Exception {
 	System.out.print("start of the test \n");
-	System.out.print(System.getProperty("testvar"));
+	System.out.print(System.getProperty("user.supp"));
+	System.out.print(System.getProperty("user.name"));
 	System.out.print("end of the test \n");
 	System.getProperties().list(System.out);
 	System.exit(0);
