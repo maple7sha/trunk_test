11c6048c68cd07aeaf3153a7254dc22b90db49e2
Maple <hao.wan@tufts.edu>
2013-09-19 10:37:13 -0400

    uname and password passed

--- src/test/java/com/trunk/selenium/trunkTestSuit/Trunk_test1_login.java
+++ src/test/java/com/trunk/selenium/trunkTestSuit/Trunk_test1_login.java
@@ -23,7 +23,7 @@ public class Trunk_test1_login {
   public void testTrunkTest1() throws Exception {
 	System.out.print("start of the test \n");
 	System.out.print(System.getProperty("sakaiHostName"));
-	System.out.print(System.getProperty("sakaiHostName"));
+	System.out.print(System.getProperty("sakaiHostUsername"));
 	System.out.print(System.getProperty("sakaiHostPassword"));
 	//System.out.print(System.getProperty("param"));
 	System.out.print("\n --end of the test \n");
