560d71ba90e1e452ef4d2afa79acf25337cdc2b8
Maple <hao.wan@tufts.edu>
2013-10-17 10:08:10 -0400

    try verified if http status 404 is catched

--- src/test/java/com/trunk/selenium/trunkTestSuit/Trunk_testHelper.java
+++ src/test/java/com/trunk/selenium/trunkTestSuit/Trunk_testHelper.java
@@ -24,12 +24,15 @@ public class Trunk_testHelper {
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get(baseUrl + "favicon.icosadf");
 
+		
 	    String str = "";
-		/*try {
-		      assertTrue(isElementPresent(By.xpath("(//img[@src='https://trunk-stage.tufts.edu/favicon.ico'])"), driver));
+		
+
+		try {
+		      assertTrue(driver.findElement(By.xpath("(//h1[contains(text(), 'HTTP Status 404')])"))!=null);
 		} catch (Error e) {
 		      str = e.toString();
-		}*/
+		}
 
 		/* lesson learned: must know to switch to the frame!!
 		 interestingly, we have to make it re-login for the script to proceed */
