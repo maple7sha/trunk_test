68d024039c29f5a60d4735f55fe9140150290870
Maple <hao.wan@tufts.edu>
2014-03-11 16:18:53 -0400

    fix trivial error

--- src/test/java/com/trunk/selenium/trunkTestSuit/Trunk_testHelper.java
+++ src/test/java/com/trunk/selenium/trunkTestSuit/Trunk_testHelper.java
@@ -32,7 +32,7 @@ public class Trunk_testHelper {
 		
 		// switch to main frame to start testing
 	    driver.get(baseUrl + "/xsl-portal");
-	    // driver.switchTo().frame(0);
+	    driver.switchTo().frame(0);
 
 	    // verify home page info
 		try {
@@ -45,7 +45,7 @@ public class Trunk_testHelper {
 		}
 
 		// login
-	    //driver.findElement(By.id("eid")).clear();
+	    driver.findElement(By.id("eid")).clear();
 	    driver.findElement(By.id("eid")).sendKeys("wrong uname");
 	    driver.findElement(By.id("pw")).clear();
 	    driver.findElement(By.id("pw")).sendKeys("wrong password");
