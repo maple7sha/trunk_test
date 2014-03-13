aa317cb88370db66571bdc95e31be658071a0c4b
HaoWan <wanhao.maple@gmail.com>
2014-03-13 15:49:00 -0400

    fallback

--- src/test/java/com/trunk/selenium/trunkTestSuit/Trunk_testHelper.java
+++ src/test/java/com/trunk/selenium/trunkTestSuit/Trunk_testHelper.java
@@ -27,12 +27,12 @@ public class Trunk_testHelper {
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get(baseUrl + "favicon.ico");
 		if(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*Status report[\\s\\S]*$")){
-                  str += "favicon NOT FOUND";
+			str += "favicon NOT FOUND";
 		}
 		
 		// switch to main frame to start testing
 	    driver.get(baseUrl + "/xsl-portal");
-	    driver.switchTo().frame(0);
+	    //driver.switchTo().frame(0);
 
 	    // verify home page info
 		try {
@@ -45,7 +45,7 @@ public class Trunk_testHelper {
 		}
 
 		// login
-	    driver.findElement(By.id("eid")).clear();
+	    //driver.findElement(By.id("eid")).clear();
 	    driver.findElement(By.id("eid")).sendKeys("wrong uname");
 	    driver.findElement(By.id("pw")).clear();
 	    driver.findElement(By.id("pw")).sendKeys("wrong password");
