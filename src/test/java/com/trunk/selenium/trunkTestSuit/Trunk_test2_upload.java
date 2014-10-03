b9b5b7c7d91c4ae8bc5bc1fbb508d24dc8abc75e
HaoWan <wanhao.maple@gmail.com>
2014-10-03 11:22:25 -0400

    assert

--- src/test/java/com/trunk/selenium/trunkTestSuit/Trunk_test2_upload.java
+++ src/test/java/com/trunk/selenium/trunkTestSuit/Trunk_test2_upload.java
@@ -23,16 +23,11 @@ public class Trunk_test2_upload {private HtmlUnitDriver driver;
   public void testTrunkTest2() throws Exception {
 	verificationErrors.append(key.login(baseUrl, driver));
     
-    driver.findElement(By.xpath("(//span[contains(text(), 'Resources')])")).click();
-    //driver.findElement(By.xpath("(//li[contains(text(), 'Add')])")).click();
-    // Upload Files 
-    //driver.switchTo().frame(0);
-    // Trigger the dropdown menu 
-    //driver.switchTo().frame(driver.findElement(By.id("Main3203b183x6c2cx4311x9c5dx0fe4d9f35c16")));
-    
+    driver.findElement(By.xpath("(//span[contains(text(), 'Resources')])")).click();    
     driver.switchTo().frame(driver.findElement(By.xpath("(//iframe[contains(text(), '')])")));
-    Actions builder = new Actions(driver); 
     
+    // Handle hidden menus
+    Actions builder = new Actions(driver); 
     WebElement menu=driver.findElement(By.xpath("(//li[contains(text(), 'Add')])"));
     builder.moveToElement(menu).build().perform();
     // have to click the menu after move to the right location!
@@ -41,22 +36,17 @@ public class Trunk_test2_upload {private HtmlUnitDriver driver;
     upload.click();
     // upload files 
     driver.findElement(By.id("content_0")).sendKeys(key.get_sample_txt());
-    /*driver.findElement(By.id("description_0")).clear();
-    driver.findElement(By.id("description_0")).sendKeys(key.get_uploadtxt_text());
-    driver.findElement(By.id("newcopyright_0")).clear();
-    driver.findElement(By.id("newcopyright_0")).sendKeys(key.get_cpright_text());
-    */
     driver.findElement(By.id("saveChanges")).click();
     
     String xpath_to_txtaddr = String.format("//a[contains(text(), '%s')]", key.get_sample_txt());
-    
     // verify that files are there 
     try {
         assertTrue(key.isElementPresent(By.xpath(xpath_to_txtaddr), driver));
     } catch (Error e) {
-        verificationErrors.append(e.toString());
+        String err= e.toString() + "Uploaded text not present";
+        verificationErrors.append(err);
     }    
-    
+    /*
     // delete all uploaded files 
     driver.findElement(By.id("selectall")).click();
     ((JavascriptExecutor)driver).executeScript("javascript:document.getElementById('sakai_action').value='doMultiItemDispatch';document.getElementById('rt_action').value='delete';document.getElementById('showForm').submit();");
@@ -70,13 +60,7 @@ public class Trunk_test2_upload {private HtmlUnitDriver driver;
     if(key.isElementPresent(By.xpath(xpath_to_txtaddr), driver)){
     	verificationErrors.append("\n line 88, Trunk_test2_upload, program should deleted all uploaded files \n");
     }
-    /*
-    try {
-        assertFalse(key.isElementPresent(By.xpath(xpath_to_txtaddr), driver));
-    } catch (Error e) {   
-    	verificationErrors.append(e.toString());
-    }
-    */
+      */
     verificationErrors.append(key.logout(driver));
   }
 
