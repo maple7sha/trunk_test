a5e394c5189dc9b36000490ea321711927096344
maple_shadow <hao.wan@tufts.edu>
2014-10-16 10:44:17 -0400

    break up membership and mysettings into two test classes

---
+++ src/test/java/com/trunk/selenium/trunkTestSuit/Trunk_test6_membership.java
@@ -0,0 +1,61 @@
+package com.trunk.selenium.trunkTestSuit;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+//import static org.hamcrest.CoreMatchers.*;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.support.ui.Select;
+//import java.util.regex.Pattern;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class Trunk_test6_membership{
+  private HtmlUnitDriver driver;
+  private String baseUrl;
+  private StringBuffer verificationErrors = new StringBuffer();
+  private Trunk_testHelper key = new Trunk_testHelper();
+  
+  @Before
+  public void setUp() throws Exception {
+	driver = key.get_driver(driver);
+    baseUrl = key.get_baseUrl();
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testTrunkTest5() throws Exception {
+	verificationErrors.append(key.login(baseUrl, driver));
+    driver.findElement(By.xpath("(//span[contains(text(), 'Membership')])")).click();
+    driver.switchTo().frame(driver.findElement(By.xpath("(//iframe[contains(text(), '')])")));
+
+    driver.findElement(By.linkText("Joinable Sites")).click();
+    driver.findElement(By.linkText("Join")).click();
+    driver.findElement(By.linkText("My Current Sites")).click();
+    driver.findElement(By.id("check-1")).click();
+    driver.findElement(By.name("eventSubmit_doGoto_unjoinconfirm")).click();
+    driver.findElement(By.name("eventSubmit_doGoto_unjoinyes")).click();
+    // test preferences
+    driver.switchTo().defaultContent();
+    driver.findElement(By.cssSelector("a.icon-sakai-preferences > span")).click();
+    driver.switchTo().frame(0);
+    Thread.sleep(5000);
+    driver.findElement(By.cssSelector("img[alt=\"Move selected\"]")).click();
+    driver.findElement(By.cssSelector("img[alt=\"Remove all\"]")).click();
+    driver.switchTo().defaultContent();
+    driver.findElement(By.cssSelector("a.icon-sakai-singleuser > span")).click();
+    driver.findElement(By.cssSelector("a.icon-sakai-sitesetup > span")).click();
+      
+    driver.switchTo().defaultContent();
+    verificationErrors.append(key.logout(driver));
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+}
