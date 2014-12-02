ec3127b2cfbd40c7a447435e9d90ce7603d0956f
HaoWan <wanhao.maple@gmail.com>
2014-12-02 11:56:51 -0500

    open all tests

--- src/test/java/com/trunk/selenium/trunkTestSuit/Test_suite.java
+++ src/test/java/com/trunk/selenium/trunkTestSuit/Test_suite.java
@@ -4,13 +4,13 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-	//Trunk_test1_login.class,
+	Trunk_test1_login.class,
 	Trunk_test2_upload.class
-	/*Trunk_test3_tuftsNow.class,
+	Trunk_test3_tuftsNow.class,
 	Trunk_test4_communication.class,
 	Trunk_test5_mysetting.class,
     Trunk_test6_membership.class,
-    Trunk_test7_preference.class*/
+    Trunk_test7_preference.class
 })
 
 public class Test_suite{
